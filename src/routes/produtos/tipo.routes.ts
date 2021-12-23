0074bc9de5faff9bd7ea547c9a7cbac46eae2d0f
Thiask <thiask@MacBook-Pro-de-Thiask.local>
2021-12-23 09:53:24 -0300

    add nova rota

--- src/routes/produtos/tipo.routes.ts
+++ src/routes/produtos/tipo.routes.ts
@@ -10,9 +10,25 @@ router_tipo.post('/cadastrar', async (req, res) => {
 
 router_tipo.get('/listar', async (req, res) => {
     const result = await tipo.findAll({
-        attributes: [['id', 'code'], ['descricao', 'name']] 
+        attributes: [['id', 'code'], ['descricao', 'name']]
     });
     res.json(result);
 })
+
+router_tipo.put('/alterar', async (req, res) => {
+    const result = await tipo.update(
+        req.body,
+        {
+            where: {
+                id: req.body.id
+            }
+        });
+    res.json(result);
+});
+
+router_tipo.delete('/deletar/:id', async (req, res) => {
+    const result = await tipo.destroy({ where: { id: req.params.id } });
+    res.json(result);
+});
 
 export { router_tipo };
