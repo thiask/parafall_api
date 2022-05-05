1aa87bdb955643e9711daf7c5509bb45d7d3828a
thiask <thiask@gmail.com>
2022-05-05 18:59:19 -0300

    alter valor

--- src/routes/dash_board.routes.ts
+++ src/routes/dash_board.routes.ts
@@ -19,7 +19,7 @@ router_dash_board.get('/estatistics/vendas/:dataInicial/:dataFinal/:idCliente', 
     INNER JOIN itens_vendas on itens_vendas.idVenda = vendas.id
     INNER JOIN produtos on produtos.id = itens_vendas.idProduto
     INNER JOIN clientes on clientes.id = vendas.idCliente
-    WHERE vendas.createdAt BETWEEN '${req.params.dataInicial}' AND '${req.params.dataFinal}' ${getIdClient()}
+    WHERE vendas.createdAt BETWEEN '${req.params.dataInicial}' AND '${req.params.dataFinal} 23:59' ${getIdClient()}
     group by produtos.id`;
 
     const result = await sequelize.query(sql);
